34ec270e61e61ffdfa787ee9d9b888ca25ab24ed
ArielYat <yat20047@gmail.com>
2022-01-26 21:24:45 +0200

    Bugfix: - add try and catch to make sticker

--- ModulesImmediate/HandleStickers.js
+++ ModulesImmediate/HandleStickers.js
@@ -10,13 +10,25 @@ export class HSt {
         const messageType = message.quotedMsgObj ? message.quotedMsgObj.type : message.type;
         message = message.quotedMsgObj ? message.quotedMsgObj : message;
         const noCrop = !!bodyText.match(HL.getGroupLang(groupsDict, chatID, "crop_sticker"));
-        if (messageType === "image") {
-            const mediaData = await client.decryptMedia(message);
-            await client.sendImageAsSticker(chatID, mediaData, {author: "ג'ון האגדי", pack: "חצול", keepScale: noCrop});
-        } else if (messageType === "video") {
-            const mediaData = await client.decryptMedia(message);
-            await client.sendMp4AsSticker(chatID, mediaData, {crop: !noCrop}, {author: "ג'ון האגדי", pack: "חצול", keepScale: noCrop});
-        } else await client.reply(chatID, HL.getGroupLang(groupsDict, chatID, "not_sticker_material_error"), messageID);
+        try {
+            if (messageType === "image") {
+                const mediaData = await client.decryptMedia(message);
+                await client.sendImageAsSticker(chatID, mediaData, {
+                    author: "ג'ון האגדי",
+                    pack: "חצול",
+                    keepScale: noCrop
+                });
+            } else if (messageType === "video") {
+                const mediaData = await client.decryptMedia(message);
+                await client.sendMp4AsSticker(chatID, mediaData, {crop: !noCrop}, {
+                    author: "ג'ון האגדי",
+                    pack: "חצול",
+                    keepScale: noCrop
+                });
+            } else await client.reply(chatID, HL.getGroupLang(groupsDict, chatID, "not_sticker_material_error"), messageID);
+        } catch (e) {
+            console.log("error occurred at sticker making")
+        }
     }
     static async createTextSticker(client, bodyText, chatID, messageID, groupsDict) {
         bodyText = bodyText.replace(HL.getGroupLang(groupsDict, chatID, "create_text_sticker"), "").trim();
