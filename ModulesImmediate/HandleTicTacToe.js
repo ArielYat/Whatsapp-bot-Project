a5212c930e4b332da97e269a81086a3b943bddad
TheBooker66 <ethan.amiran@gmail.com>
2022-02-03 17:47:25 +0200

    Update: TicTacToe Alpha!

---
+++ ModulesImmediate/HandleTicTacToe.js
@@ -0,0 +1,166 @@
+import {HL} from "../ModulesDatabase/HandleLanguage.js"
+
+export class H3T {
+    static async TicTacToe(client, bodyText, chatID, messageID, groupsDict) {
+        async function init() {
+            bodyText = bodyText.replace(HL.getGroupLang(groupsDict, chatID, "init_tic_tac_toe"));
+            bodyText = bodyText.split(",");
+            const clickedRow = bodyText[1].trim(), clickedCol = bodyText[2].trim();
+            board[clickedRow][clickedCol] = 1;
+            await client.reply(chatID, board.toString(), messageID);
+        }
+
+        async function playerMove() {
+            const catchMessages = message => message.body.match(HL.getGroupLang(groupsDict, chatID, "move_tic_tac_toe"));
+            await chatID.awaitMessages(catchMessages, {max: 1, time: 6000, errors: ['time']})
+                .then(caught => async function () {
+                    const text = caught[0].value.split(",");
+                    const clickedRow = text[0].trim(), clickedCol = text[1].trim();
+                    board[clickedRow][clickedCol] = 1;
+                    await sendMoveReply();
+                    if (await checkWin(board, clickedRow, clickedCol))
+                        await client.sendText(HL.getGroupLang(groupsDict, chatID, "win_tic_tac_toe_reply"));
+                    else {
+                        if (await checkWin(await computerMove())) {
+                            await sendMoveReply();
+                            await client.sendText(HL.getGroupLang(groupsDict, chatID, "lose_tic_tac_toe_reply"));
+                        } else {
+                            await sendMoveReply();
+                            await playerMove();
+                        }
+                    }
+                })
+                .catch(client.reply(chatID, HL.getGroupLang(groupsDict, chatID, "tic_tac_toe_time_out_error"), messageID));
+        }
+
+        async function sendMoveReply() {
+            let stringForSending = HL.getGroupLang(groupsDict, chatID, "move_tic_tac_toe_reply");
+            for (let i = 0; i < board.length; i++) {
+                for (let j = 0; j < board.length; j++) {
+                    if (board[i][j] === 0)
+                        stringForSending += " — ";
+                    else if (board[i][j] === 1)
+                        stringForSending += " ❌ ";
+                    else if (board[i][j] === 2)
+                        stringForSending += " ⭕ ";
+                }
+                stringForSending += "\n"
+            }
+            await client.sendText(stringForSending);
+        }
+
+        async function checkWin(b = board, clickedRow, clickedCol) {
+            let count = 1, colToCheck, rowToCheck;
+            colToCheck = clickedCol - 1;
+            while (colToCheck > -1) {
+                if (1 === b[clickedRow][colToCheck]) {
+                    count++;
+                    colToCheck--;
+                } else
+                    break;
+            }
+            colToCheck = clickedCol + 1;
+            while (colToCheck < maxCol) {
+                if (1 === b[clickedRow][colToCheck]) {
+                    count++;
+                    colToCheck++;
+                } else
+                    break;
+            }
+            if (count >= sequence)
+                return true;
+            count = 1;
+            rowToCheck = clickedRow - 1;
+            while (rowToCheck > -1) {
+                if (1 === b[rowToCheck][clickedCol]) {
+                    count++;
+                    rowToCheck--;
+                } else
+                    break;
+            }
+            rowToCheck = clickedRow + 1;
+            while (rowToCheck < maxRow) {
+                if (1 === b[rowToCheck][clickedCol]) {
+                    count++;
+                    rowToCheck++;
+                } else
+                    break;
+            }
+
+            if (count >= sequence)
+                return true;
+            count = 1;
+            colToCheck = clickedRow - 1;
+            rowToCheck = clickedRow - 1;
+            while (rowToCheck > -1) {
+                if (1 === b[rowToCheck][colToCheck]) {
+                    count++;
+                    colToCheck--;
+                    rowToCheck--;
+                } else
+                    break;
+            }
+            colToCheck = clickedRow + 1;
+            rowToCheck = clickedRow + 1;
+            while (rowToCheck < maxRow) {
+                if (1 === b[rowToCheck][colToCheck]) {
+                    count++;
+                    colToCheck++;
+                    rowToCheck++;
+                } else
+                    break;
+            }
+
+            if (count >= sequence)
+                return true;
+            count = 1;
+            colToCheck = clickedCol + 1;
+            rowToCheck = clickedRow - 1;
+            while (rowToCheck > -1) {
+                if (1 === b[rowToCheck][colToCheck]) {
+                    count++;
+                    colToCheck++;
+                    rowToCheck--;
+                } else
+                    break;
+            }
+            colToCheck = clickedCol + 1;
+            rowToCheck = clickedRow + 1;
+            while (rowToCheck < maxRow) {
+                if (1 === b[rowToCheck][colToCheck]) {
+                    count++;
+                    colToCheck--;
+                    rowToCheck++;
+                } else
+                    break;
+            }
+            if (count >= sequence)
+                return true;
+        }
+
+        async function computerMove() {
+            let itemsToReturn;
+            for (let i = 0; i < board.length; i++) {
+                for (let j = 0; j < board[i].length; j++) {
+                    if (board[i][j] !== 0) {
+                        let tempBoard = JSON.parse(JSON.stringify(board));
+                        tempBoard[i][j] = 2;
+                        if (await checkWin(tempBoard, i, j)) {
+                            board[i][j] = 2;
+                            return [board, i, j];
+                        } else {
+                            board[i][j] = 2;
+                            itemsToReturn = [board, i, j];
+                        }
+                    }
+                }
+            }
+            return itemsToReturn;
+        }
+
+        let maxRow = 3, maxCol = 3, sequence = 3;
+        let board = [maxRow, maxCol];
+        await init();
+        await playerMove();
+    }
+}
