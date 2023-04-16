d78964b2402a85730416b88649c5fa3eb625c477
Beery Shklar <beeryshkklar@gmail.com>
2023-04-16 13:57:53 +0300

    Add example file for api keys

---
+++ apiKeys.example.js
@@ -0,0 +1,15 @@
+module.exports = {
+	DBUrl: 'DB_URL',
+	region: 'Asia/Jerusalem',
+	originalGroup: 'ChatID',
+	secondGroup: 'ChatID',
+	configObj: {
+		// Whatsapp client configuration (ConfigObject | AdvancedConfig)
+		sessionId: 'main',
+	},
+	virusAPI: 'VIRUS_API_URL',
+	criptoAPI: 'CRIPTO_API_URL',
+	stockAPI: 'STOCK_API_URL',
+	transcriptionAPI: 'TRANSCRIPTION_API_URL',
+	stickerTemplatePath: 'path',
+}
