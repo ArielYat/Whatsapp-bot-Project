d4f2b92be76beb134fd75daeea9137b7a746bd12
ArielYat <yat20047@gmail.com>
2023-04-16 14:27:24 +0300

    entrypoint.sh fixed and added proton.txt example

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 # entrypoint.sh
 
 set -eu
-tmux new-session -s foo -d && tmux list-sessions && tmux send-keys -t foo "openvpn --config /app/jp-free-04.protonvpn.net.udp.ovpn --auth-user-pass /app/test.txt" Enter && sleep 60 && tmux capture-pane -pS foo
+tmux new-session -s foo -d && tmux list-sessions && tmux send-keys -t foo "openvpn --config /app/$(ls -a /app | grep .ovpn) --auth-user-pass /app/proton.txt" Enter && sleep 60 && tmux capture-pane -pS foo
 exec "$@"
